4815e734c160cf3a9678773809f0cd98be67b43f
Samnan ur Rehman <samnan.akhoond@gmail.com>
2014-12-19 13:18:38 +0500

    updated source files

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/ko3docs/"
+http_images_path = "/ko3docs/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/ko3docs/fonts"
+css_dir = "public/ko3docs/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/what-is-kohana/index.markdown
@@ -0,0 +1,10 @@
+---
+layout: page
+title: "what-is-kohana"
+date: 2014-12-19 13:05
+comments: true
+sharing: true
+footer: true
+---
+
+Kohana is an elegant PHP framework developed from scratch.
